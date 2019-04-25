9372531897005a4262bb4a19a8f6b5c46e8990b7
user.email <ngovanthinh1617@gmail.com>
2019-04-25 16:27:32 +0700

    Finish chapter 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
